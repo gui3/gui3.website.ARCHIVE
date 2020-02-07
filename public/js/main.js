ee24c70ceb10978d7f4561f948612241d49462d6
gui3 <guillaume.silvent@hotmail.com>
2020-02-07 18:19:01 +0100

    update tictactoe

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,26 @@
 
 function toggleSidebar () {
-  let sidebar = document.getElementById("sidebar");
+  const sidebar = document.getElementById('sidebar')
   switch (sidebar.style.display) {
-    case "none":
-      sidebar.style.display = "flex";
-      break;
+    case 'none':
+      sidebar.style.display = 'flex'
+      break
     default:
-      sidebar.style.display = "none";
+      sidebar.style.display = 'none'
   }
 }
 
-function listen(elem, evnt, func) {
-    if (elem.addEventListener)  // W3C DOM
-        elem.addEventListener(evnt,func,false);
-    else if (elem.attachEvent) { // IE DOM
-         var r = elem.attachEvent("on"+evnt, func);
-         return r;
-    }
-    else console.log('I\'m sorry Dave, I\'m afraid I can\'t do that.');
+function listen (elem, evnt, func) {
+  if (elem.addEventListener) { // W3C DOM
+    elem.addEventListener(evnt, func, false)
+  } else if (elem.attachEvent) { // IE DOM
+    var r = elem.attachEvent('on' + evnt, func)
+    return r
+  } else console.log('I\'m sorry Dave, I\'m afraid I can\'t do that.')
+}
+
+function clearElement (elemt) {
+  while (elemt.firstChild) {
+    elemt.removeChild(elemt.firstChild)
+  }
 }
--- public/js/minigames/tictactoe.js
+++ public/js/minigames/tictactoe.js
@@ -1,78 +1,168 @@
 
-listen(window, "load", () => {
+listen(window, 'load', () => {
+  const tttCells = [[], [], []]
+  let over = false;
 
-  let tttCells = [[],[],[]];
-
-  //generic functions ------------------
+  // generic functions ------------------
   function message (text) {
-    let messageBox = document.getElementById("tttMessages");
+    const messageBox = document.getElementById('tttMessages')
+    clearElement(messageBox)
     messageBox.appendChild(
-      document.createTextNode("\n" + text)
-    );
-    messageBox.scrollTop = messageBox.scrollHeight;
+      document.createTextNode(text)
+    )
+    //messageBox.scrollTop = messageBox.scrollHeight
   }
 
-  function getPlayerSymbol () {
-    return "O";
+  function getPlayerSymbol (player) {
+    const symbol = player === 1 ? 'O' : 'X'
+    return symbol
   }
 
-  //function play ----------------------
+  // function play ----------------------
   function play (evnt) {
-    let btn = event.target;
-    let cell = tttCells[btn.x][btn.y];
-    if (checkGameOver()) {
-      resetGame();
-    } else if (cell.state != "") {
-      message("Cette cellule est déjà prise, finis la partie!")
+    const btn = evnt.target
+    const cell = tttCells[btn.x][btn.y]
+    if (over) {
+      resetGame()
+    } else if (cell.state !== '') {
+      message('Cette cellule est déjà prise !')
     } else {
-      cell.state = "O";
-      btn.innerHTML = getPlayerSymbol();
-      checkGameOver();
-      botTurn();
-      checkGameOver();
+      setCell(cell, getPlayerSymbol(1))
+      if (!checkGameOver()) {
+        botTurn()
+        checkGameOver()
+      } else {
+        over = true
+      }
     }
-  };
+  }
+
+  function resetGame () {
+    over = false
+    tttCells.forEach(col => {
+      col.forEach(cell => {
+        setCell(cell, '')
+      })
+    })
+  }
+
+  function setCell (cell, symbol) {
+    cell.state = symbol
+    cell.button.innerHTML = symbol
+  }
+
+  function getAllLines () {
+    const results = []
+    const diagUp = []
+    const diagDown = [];
+    [0, 1, 2].forEach(num => {
+      // check rows -----------
+      results.push([
+        tttCells[0][num],
+        tttCells[1][num],
+        tttCells[2][num]])
+      // check columns --------
+      results.push([
+        tttCells[num][0],
+        tttCells[num][1],
+        tttCells[num][2]])
+      // check diags ----------
+      diagUp.push(tttCells[num][num])
+      diagDown.push(tttCells[num][2 - num])
+    })
+    results.push(diagUp)
+    results.push(diagDown)
+
+    return results
+  }
 
   function checkGameOver () {
-    //check rows
-    [0,1,2].forEach(row => {
-      let consecutive = 0;
-      let cursor = null;
-      [0,1,2].forEach(x => {
-        if (cursor == tttCells[x][row].state) {
-          ++consecutive;
-        }else {
-          consecutive = 0;
-        };
-        cursor = tttCells[x][row].state;
-        console.log(cursor + ":"+consecutive)
+    const lines = getAllLines()
+    let over = true
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i]
+      if (line[0].state === '' ||
+          line[1].state === '' ||
+          line[2].state === '') {
+        over = false
+      } else {
+        if (line[0].state === line[1].state &&
+            line[0].state === line[2].state) {
+          winGame(line)
+          return true
+        }
+      }
+    }
+    if (over) return true
+    else return false
+  }
+
+  function botTurn () { // AI playing --------------
+    const cells = []
+    const lines = getAllLines()
+    lines.forEach(line => {
+      line.forEach(cell => {
+        cell.score = 0
+        cells.push(cell)
       })
-      if (consecutive == 2 && cursor != "") winGame(cursor)
-    });
+    })
+
+    function getLineScore (line) {
+      line.score = 0
+      line.forEach(cell => {
+        if (cell.state === 0) {
+          line.score + 2
+        } else if (cell.state === getPlayerSymbol(2)) {
+          line.score += 5
+        } else {
+          line.score -= 2
+        }
+      })
+      line.forEach(cell => {
+        if (cell.state === '') cell.score += line.score
+        // console.log(cell)
+      })
+    }
+
+    lines.forEach(getLineScore)
+    const bestLine = lines.reduce((a, b) => a.score > b.score ? a : b)
+    const bestCell = bestLine.reduce((a, b) => a.score > b.score ? a : b)
+
+    console.log(bestCell)
+    setCell(bestCell, getPlayerSymbol(2))
   }
 
-  function botTurn () {
+  function winGame (line) {
+    const text = 'les ' + line[0].state +
+      ' ont gagné ! \ncliquez pour rejouer'
+    message(text)
+    /*
+    setTimeout(_ => {
+      alert(text)
+    }, 500)
+    */
+  };
 
-  }
-
-  function winGame (symbol) {
-    console.log(symbol + "a gagne")
-  }
-
-  //getting the cells ------------------
-  [0,1,2].forEach(x => {
-    [0,1,2].forEach(y => {
-      let btn = document.getElementById("ttt"+x+"."+y);
-      btn.x = x;
-      btn.y = y;
-      btn.addEventListener("click", play);
-      tttCells[x][y] = {
+  // getting the cells ------------------
+  [0, 1, 2].forEach(x => {
+    [0, 1, 2].forEach(y => {
+      const btn = document.getElementById('ttt' + x + '.' + y)
+      btn.x = x
+      btn.y = y
+      btn.addEventListener('click', play)
+      const cell = {
         button: btn,
-        state: btn.innerHTML
-      };
-    });
-  });
-  console.log(tttCells);
-
-
+        state: ''
+      }
+      setCell(
+        cell,
+        btn.innerHTML === 'O'
+          ? getPlayerSymbol(1)
+          : btn.innerHTML
+            ? getPlayerSymbol(2)
+            : '')
+      tttCells[x][y] = cell
+    })
+  })
+  console.log(tttCells)
 })
